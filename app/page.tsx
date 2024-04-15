ef9c8f3184ce5766bd6b8cee3f270e6c6e9584e9
seonghwanjo <joseonghwan3021@gmail.com>
2024-04-15 18:17:40 +0900

    3.5. Responsive Modifieres

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,15 @@
 export default function Home() {
   return (
-    <main className="h-screen flex items-center justify-center bg-gray-100 dark:bg-gray-800">
-      <div className="w-96 h-44 bg-white rounded-lg shadow-xl px-8 py-auto flex flex-col justify-evenly dark:bg-gray-600 max-w-screen-md">
-        <div className="flex w-full justify-between items-center">
-          <div className="flex flex-col">
-            <span className="text-gray-500 text-sm font-light dark:text-gray-300">
-              In transit
-            </span>
-            <h1 className="text-3xl font-bold dark:text-white">Coolblue</h1>
-          </div>
-          <div className="bg-orange-600 rounded-full w-10 h-10 flex flex-col justify-center items-center text-white">
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              strokeWidth="1"
-              stroke="currentColor"
-              className="w-5 h-5"
-            >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M8.25 18.75a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h6m-9 0H3.375a1.125 1.125 0 0 1-1.125-1.125V14.25m17.25 4.5a1.5 1.5 0 0 1-3 0m3 0a1.5 1.5 0 0 0-3 0m3 0h1.125c.621 0 1.129-.504 1.09-1.124a17.902 17.902 0 0 0-3.213-9.193 2.056 2.056 0 0 0-1.58-.86H14.25M16.5 18.75h-2.25m0-11.177v-.958c0-.568-.422-1.048-.987-1.106a48.554 48.554 0 0 0-10.026 0 1.106 1.106 0 0 0-.987 1.106v7.635m12-6.677v6.677m0 4.5v-4.5m0 0h-12"
-              />
-            </svg>
-          </div>
-        </div>
-        <div className="flex items-center space-x-2">
-          <span className="bg-green-500 text-white px-2 py-0.5 font-normal rounded-full text-sm cursor-pointer hover:bg-green-600 active:bg-green-700 hover:scale-110 transition-all">
-            Today
-          </span>
-          <span className="text-gray-600 dark:text-gray-300">9:30-10:30</span>
-        </div>
-        <div className="relative">
-          <div className="w-full bg-slate-200 rounded-full h-3 absolute" />
-          <div className="w-3/4 bg-orange-600 rounded-full h-3 absolute" />
-        </div>
-        <div className="flex justify-between text-gray-400 text-xs dark:text-gray-200">
-          <span>Expected</span>
-          <span>Sorting centre</span>
-          <span>In transit</span>
-          <span className="dark:text-gray-400 text-gray-300">Delivered</span>
-        </div>
+    <main className="h-screen flex items-center justify-center bg-gray-100 p-5">
+      <div className="bg-white shadow-lg p-5 rounded-3xl w-full max-w-screen-sm flex flex-col gap-2.5">
+        <input
+          type="text"
+          className="w-full rounded-full py-2.5 bg-gray-200 px-4 outline-none ring ring-transparent focus:ring-orange-500 focus:ring-offset-2 transition-shadow duration-150 placeholder:drop-shadow-sm"
+          placeholder="Search here..."
+        />
+        <button className="bg-black bg-opacity-50 focus:bg-opacity-100 text-white font-medium py-2 rounded-full active:scale-95 focus:scale-95 outline-none transition-transform">
+          Search
+        </button>
       </div>
     </main>
   );
