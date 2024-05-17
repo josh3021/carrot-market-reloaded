29fbdb06495b77c98468c067af902a4968e6aec5
seonghwanjo <joseonghwan3021@gmail.com>
2024-05-17 11:22:11 +0900

    #15.1 Models

---
+++ prisma/migrations/20240517022106_chatroom_and_message/migration.sql
@@ -0,0 +1,32 @@
+-- CreateTable
+CREATE TABLE "ChatRoom" (
+    "id" TEXT NOT NULL PRIMARY KEY,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL
+);
+
+-- CreateTable
+CREATE TABLE "Message" (
+    "id" INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+    "payload" TEXT NOT NULL,
+    "room_id" TEXT NOT NULL,
+    "user_id" INTEGER NOT NULL,
+    "created_at" DATETIME NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    "updated_at" DATETIME NOT NULL,
+    CONSTRAINT "Message_room_id_fkey" FOREIGN KEY ("room_id") REFERENCES "ChatRoom" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "Message_user_id_fkey" FOREIGN KEY ("user_id") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateTable
+CREATE TABLE "_ChatRoomToUser" (
+    "A" TEXT NOT NULL,
+    "B" INTEGER NOT NULL,
+    CONSTRAINT "_ChatRoomToUser_A_fkey" FOREIGN KEY ("A") REFERENCES "ChatRoom" ("id") ON DELETE CASCADE ON UPDATE CASCADE,
+    CONSTRAINT "_ChatRoomToUser_B_fkey" FOREIGN KEY ("B") REFERENCES "User" ("id") ON DELETE CASCADE ON UPDATE CASCADE
+);
+
+-- CreateIndex
+CREATE UNIQUE INDEX "_ChatRoomToUser_AB_unique" ON "_ChatRoomToUser"("A", "B");
+
+-- CreateIndex
+CREATE INDEX "_ChatRoomToUser_B_index" ON "_ChatRoomToUser"("B");
